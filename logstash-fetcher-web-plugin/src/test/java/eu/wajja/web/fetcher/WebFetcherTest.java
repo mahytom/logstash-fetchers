5b22058eb26fc9b624e7de0ac8c4da004104e490
tom <mail@tommahy.com>
2019-09-11 20:11:14 +0200

    unit test placeholder

--- logstash-fetcher-web-plugin/src/test/java/eu/wajja/web/fetcher/WebFetcherTest.java
+++ logstash-fetcher-web-plugin/src/test/java/eu/wajja/web/fetcher/WebFetcherTest.java
@@ -1,39 +1,43 @@
 package eu.wajja.web.fetcher;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Consumer;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.logstash.plugins.ConfigurationImpl;
 
 import co.elastic.logstash.api.Configuration;
-import eu.wajja.web.fetcher.WebFetcher;
 
 public class WebFetcherTest {
 
 	@Test
-	public void testWebFetcher() {
+	public void testWebFetcher() throws IOException {
 
 		Map<String, Object> configValues = new HashMap<>();
-		configValues.put(WebFetcher.CONFIG_URLS.name(), Arrays.asList("https://euraxess.ec.europa.eu"));
-		configValues.put(WebFetcher.CONFIG_DATA_FOLDER.name(), "C:/data/");
+		configValues.put(WebFetcher.CONFIG_URLS.name(), Arrays.asList("http://test_node_1"));
+		configValues.put(WebFetcher.CONFIG_DATA_FOLDER.name(), Files.createTempDirectory(UUID.randomUUID().toString()));
+		configValues.put(WebFetcher.CONFIG_EXCLUDE.name(), Arrays.asList("css"));
+		configValues.put(WebFetcher.CONFIG_THRAD_POOL_SIZE.name(), 1);
+		configValues.put(WebFetcher.CONFIG_REFRESH_INTERVAL.name(), 1);
+
 		Configuration config = new ConfigurationImpl(configValues);
-		WebFetcher input = new WebFetcher("test-id", config, null);
+		WebFetcher webFetcher = new WebFetcher("test-id", config, null);
+
 		TestConsumer testConsumer = new TestConsumer();
-		input.start(testConsumer);
+		webFetcher.start(testConsumer);
 
 		List<Map<String, Object>> events = testConsumer.getEvents();
-//		Assert.assertEquals(eventCount, events.size());
-//		for (int k = 1; k <= events.size(); k++) {
-//			Assert.assertEquals(prefix + " " + StringUtils.center(k + " of " + eventCount, 20),
-//					events.get(k - 1).get("message"));
-//		}
+
+		Assert.assertEquals(1, events.size());
+
 	}
 
 	private static class TestConsumer implements Consumer<Map<String, Object>> {
