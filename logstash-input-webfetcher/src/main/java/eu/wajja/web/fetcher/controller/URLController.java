e175e9333e8d48e1b9a1ae080d00696817f17e63
mahytom <mail@tommahy.com>
2020-09-08 09:47:22 +0200

    logging queue to elastic for less mem consumption

--- logstash-input-webfetcher/src/main/java/eu/wajja/web/fetcher/controller/URLController.java
+++ logstash-input-webfetcher/src/main/java/eu/wajja/web/fetcher/controller/URLController.java
@@ -7,6 +7,7 @@ import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.util.Base64;
 
 import javax.net.ssl.SSLException;
 
@@ -14,6 +15,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eu.wajja.web.fetcher.elasticsearch.ElasticSearchService;
 import eu.wajja.web.fetcher.model.Result;
 
 public class URLController {
@@ -26,10 +28,10 @@ public class URLController {
 	private String referer;
 	private String waitForCssSelector;
 	private Integer maxWaitForCssSelector;
-
+	private ElasticSearchService elasticSearchService;
 	private WebDriverController webDriverController = new WebDriverController();
 
-	public URLController(Proxy proxy, Long timeout, String userAgent, String referer, String waitForCssSelector, Integer maxWaitForCssSelector) {
+	public URLController(ElasticSearchService elasticSearchService, Proxy proxy, Long timeout, String userAgent, String referer, String waitForCssSelector, Integer maxWaitForCssSelector) {
 
 		this.proxy = proxy;
 		this.timeout = timeout;
@@ -37,11 +39,16 @@ public class URLController {
 		this.referer = referer;
 		this.waitForCssSelector = waitForCssSelector;
 		this.maxWaitForCssSelector = maxWaitForCssSelector;
+		this.elasticSearchService = elasticSearchService;
 	}
 
-	public Result getURL(String currentUrl, String initialUrl, String chromeDriver) {
+	public Result getURL(String index, String currentUrl, String initialUrl, String chromeDriver) {
 
 		Result result = new Result();
+		result.setUrl(currentUrl);
+		result.setRootUrl(initialUrl);
+		result.setCode(404);
+		
 		HttpURLConnection httpURLConnection = null;
 
 		try {
@@ -59,6 +66,7 @@ public class URLController {
 			httpURLConnection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
 			httpURLConnection.addRequestProperty("User-Agent", userAgent);
 			httpURLConnection.addRequestProperty("Referer", referer);
+			httpURLConnection.setRequestMethod("HEAD");
 
 			httpURLConnection.connect();
 
@@ -67,23 +75,34 @@ public class URLController {
 
 			result.setCode(code);
 			result.setMessage(message);
-			result.setUrl(currentUrl);
-			result.setRootUrl(initialUrl);
+			result.setContentType(httpURLConnection.getContentType());
 
 			if (code == HttpURLConnection.HTTP_OK) {
-				/*
-				 * If pdf, do not use the webdriver, which was only used intitially to handle js
-				 */
-				if (httpURLConnection.getContentType().equals("application/pdf") || currentUrl.substring(currentUrl.length() - 3).equalsIgnoreCase("pdf")) {
 
-					LOGGER.debug("Found pdf, downloading {}", currentUrl);
-					result.setContent(IOUtils.toByteArray(httpURLConnection.getInputStream()));
+				Integer length = httpURLConnection.getContentLength();
+				result.setLength(length);
+
+				if (length > 0 && elasticSearchService.existsInIndexWithSize(currentUrl, length, index)) {
+					result = elasticSearchService.getFromIndex(currentUrl, index);
 
 				} else {
 
-					closeConnection(httpURLConnection);
-					result.setHeaders(httpURLConnection.getHeaderFields());
-					webDriverController.getURL(result, chromeDriver, waitForCssSelector, maxWaitForCssSelector);
+					/*
+					 * If pdf, do not use the webdriver, which was only used intitially to handle js
+					 */
+					if (httpURLConnection.getContentType().equals("application/pdf") || currentUrl.substring(currentUrl.length() - 3).equalsIgnoreCase("pdf")) {
+
+						LOGGER.debug("Found pdf, downloading {}", currentUrl);
+						result.setHeaders(httpURLConnection.getHeaderFields());
+						result.setContent(downloadContent(currentUrl));
+
+					} else {
+
+						closeConnection(httpURLConnection);
+						result.setHeaders(httpURLConnection.getHeaderFields());
+						byte[] bytes = webDriverController.getURL(result.getUrl(), chromeDriver, waitForCssSelector, maxWaitForCssSelector);
+						result.setContent(bytes);
+					}
 
 				}
 
@@ -93,7 +112,7 @@ public class URLController {
 				closeConnection(httpURLConnection);
 
 				LOGGER.debug("Redirect needed to :  {}", newUrl);
-				return getURL(newUrl, initialUrl, chromeDriver);
+				return getURL(index, newUrl, initialUrl, chromeDriver);
 
 			} else {
 				LOGGER.warn("Failed To Read status {}, url {}, message {}", code, url, message);
@@ -123,6 +142,56 @@ public class URLController {
 		}
 
 		return result;
+	}
+
+	private byte[] downloadContent(String currentUrl) {
+
+		HttpURLConnection httpURLConnection = null;
+
+		try {
+
+			URL url = this.createUrl(currentUrl);
+
+			if (proxy == null) {
+				httpURLConnection = (HttpURLConnection) url.openConnection();
+			} else {
+				httpURLConnection = (HttpURLConnection) url.openConnection(proxy);
+			}
+
+			httpURLConnection.setConnectTimeout(timeout.intValue());
+			httpURLConnection.setReadTimeout(timeout.intValue());
+			httpURLConnection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+			httpURLConnection.addRequestProperty("User-Agent", userAgent);
+			httpURLConnection.addRequestProperty("Referer", referer);
+
+			httpURLConnection.connect();
+
+			try (InputStream inputStream = httpURLConnection.getInputStream()) {
+				return IOUtils.toByteArray(inputStream);
+			}
+
+		} catch (SocketTimeoutException e) {
+
+			LOGGER.warn("Thread url {}, sleeping and trying again", currentUrl);
+			closeConnection(httpURLConnection);
+
+		} catch (SSLException e) {
+
+			LOGGER.warn("Thread url {}, SSLException error, sleeping and trying again", currentUrl, e);
+			closeConnection(httpURLConnection);
+
+		} catch (MalformedURLException mue) {
+			LOGGER.error("Malformed url : {}", currentUrl, mue);
+			closeConnection(httpURLConnection);
+
+		} catch (Exception e) {
+			LOGGER.error("Failed to retrieve URL url {}", currentUrl, e);
+		} finally {
+			closeConnection(httpURLConnection);
+		}
+
+		return null;
+
 	}
 
 	private void closeConnection(HttpURLConnection httpURLConnection) {
